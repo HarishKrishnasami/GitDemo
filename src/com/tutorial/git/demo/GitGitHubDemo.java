cce88f436eff66e1f744bfa3966b5f6fe1ccf5ca
Harish <kris.harish@gmail.com>
2020-04-22 19:55:40 +0530

    first commit from eclipse - GitDemo prj

---
+++ src/com/tutorial/git/demo/GitGitHubDemo.java
@@ -0,0 +1,11 @@
+package com.tutorial.git.demo;
+
+public class GitGitHubDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Git & GitHub tutorial");
+
+	}
+
+}
